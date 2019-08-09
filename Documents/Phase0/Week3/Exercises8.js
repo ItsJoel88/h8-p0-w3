5f7f5241ad4e6428792ea341c6619133f06a0525
Imanuel Nusantara Jodi <48381675+ItsJoel88@users.noreply.github.com>
2019-08-09 15:59:53 +0700

    Update Exercises8.js

--- Documents/Phase0/Week3/Exercises8.js
+++ Documents/Phase0/Week3/Exercises8.js
@@ -1,14 +1,39 @@
-function pasanganTerbesar(num){
-  
-    var b=num+''
-    var c=b.split('').map(Number)
-    var tampung=c.indexOf(Math.max(...c))
-    var angkaBesar1=c.splice(tampung,1)+''
-    var tampung1=c.indexOf(Math.max(...c))
-    var angkaBesar2=c.splice(tampung1,1)+''
-    return angkaBesar1+angkaBesar2
+function targetTerdekat(num){
+    var angkaString = num+''
+    var c=angkaString.split('').map(Number)
+    var tampung = c.indexOf(Math.max(...c))
+    var kiri1 = angkaString[tampung-1]+angkaString[tampung]
+    var kanan1 = angkaString[tampung]+angkaString[tampung+1]
+    /*==========================================================*/
+    if(angkaString[tampung+1] !== undefined && angkaString[tampung-1] !== undefined){
+        if(kiri1>kanan1){
+            return kiri1
+        }else{
+            return kanan1
+        }
+    }else{
+        c.splice(tampung,1)
+        var angkaTerbesar2 = angkaString[c.indexOf(Math.max(...c))]
+        var tampung1=angkaString.indexOf(angkaTerbesar2)
+        var kiri2 = angkaString[tampung1-1]+angkaString[tampung1]
+        var kanan2 = angkaString[tampung1]+angkaString[tampung1+1]
+        if(kiri1<kanan2 || kiri1<kiri2){
+            if(kanan2>kiri2){
+                return kanan2
+            }else{
+                return kiri2
+            }
+        }else if(angkaString[tampung-1]){
+            return kiri1
+        }else{
+            return kanan1
+        }
+    }
 }
-
-console.log(pasanganTerbesar(12345)) // 54
-console.log(pasanganTerbesar(918325105)) //98
-console.log(pasanganTerbesar(2876587263)) //88
+console.log(targetTerdekat(641573)) // 73
+console.log(targetTerdekat(12783456)) // 83
+console.log(targetTerdekat(910233)) // 91
+console.log(targetTerdekat(71856421)) // 85
+console.log(targetTerdekat(79918293)) //99
+console.log(targetTerdekat(38790)) //90
+console.log(targetTerdekat(3879)) //87
