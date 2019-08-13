db3967440de1712ff5354b79bec50d5920f3f950
Imanuel Nusantara Jodi <48381675+ItsJoel88@users.noreply.github.com>
2019-08-13 15:19:40 +0700

    update Exercises13.js
    Memperbaiki error code berdasarkan feedback kak Yusuf

--- Documents/Phase0/Week3/Exercises13.js
+++ Documents/Phase0/Week3/Exercises13.js
@@ -1,28 +1,44 @@
 function targetTerdekat(arr){
+    var index = arr.indexOf('o')
+    if(arr.indexOf('x')>arr.indexOf('o')){
+    var tampung = 0
+        for(var i =index;i<arr.length;i++){
+            if(arr[i]===''||arr[i]===' '){
+                tampung +=1
+            }
+            if(arr[i]==='x'){
+                tampung +=1
+                break
+            }
+        }
+    }
+    if(arr.indexOf('x')<arr.indexOf('o')){
+        var tampung1 = 0
+        for(var j=index;j>=0;j--){
+            if(arr[j]==='' || arr[j]===' '){
+                tampung1 +=1
+            }
+            if(arr[j]==='x'){
+                tampung1 +=1
+                break
+            }
+        }
+    }
+        
     if(arr.includes('x')===false){
         return 0
     }
-    var index=arr.indexOf('o')
-    var index1
-    var i=0
-   
-    if(arr.indexOf('o')<arr.indexOf('x')){
-        while(i<arr.length){
-            if(arr[i]==='x'){
-                index1 = arr.indexOf(arr[i])
-            }
-            i++
-        }
-        return index1-index
+    else if(tampung>tampung1 || tampung === undefined){
+        return tampung1
     }else{
-        return Math.abs(arr.lastIndexOf('x')-index)
+        return tampung
     }
-    
-}
 
+}
 console.log(targetTerdekat(['','o','','x','x'])) //2
 console.log(targetTerdekat(['x','','','o',''])) //3
 console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])) //1
 console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])) // 3
 console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])) //4
 console.log(targetTerdekat(['','','o',''])) //0
+console.log(targetTerdekat(['x','o','','x','x'])) //1
